3502bf2bbc8786a2da0d506ef247d83e4494e22f
vehhty <84939585+vehhtyy@users.noreply.github.com>
2022-05-02 14:38:44 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="PirateStream.ml - Darmowe re-transmisje meczów online, stream fame mma, ksw, mma vip, highlive, royal division i wiele więcej...">
+<meta name="keywords" content="filmy, retransmisje, piratesite, re-transmisje, royal division, piratestream.ml, seriale, online, filmy online, filmy bez limitu, filmy za darmo, filmy z lektorem, kino online, premiery kinowe, nowości filmowe">
+<title>PirateStream.ml - retransmisje ^_^</title>
+<link rel="Stylesheet" href="css/style.css">
+<link rel="shortcut icon" href="https://i.imgur.com/edUo82t.png">
+<link rel="Stylesheet" href="css/fontello.css">
+<link rel="stylesheet" href="css/animation.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+</head>
+<body>
+<div id="nav">
+<a class="logo" href="/"><img src="20220418_140617.png" style="width: 275px;"></a>
+<!-- <li style="border-radius:20px;"><a href="https://paypal.me/domanu"><i class="icon-money"></i><span class="menu_content" style="color:red;">Donate</span></a></li> -->
+<!-- <li style="border-radius:20px;"><a href="https://discord.gg/piratesite"> <i class="icon-chat-1"></i> <span class="menu_content">Discord</span></a></li> -->
+</ul>
+</div>
+<div id="con">
+<div id="film">
+
+
+<iframe src="player.php" width="100%" height="100%" frameborder="0" scrolling="no" autoplay="1" allowfullscreen="" id="rk"></iframe>
+</div>
+<div id="chat">
+
+<iframe src="https://www.twitch.tv/embed/vehhty_/chat?darkpopout&parent=piratestream.ml"></iframe>
+</div>
+</div>
+<div id="footer">
+<ul id="navigation1" style="margin:auto !important;">
+<!-- <li style="border-radius:20px;"><a href="https://discord.gg/piratesite"><img style="height: 70px;" src="/discord.png"></a></li> -->
+<!-- <li style="border-radius:20px;"><a href="https://paypal.me/domanu"><img style="height: 50px;margin-top: 10px;" src="/donate.png"></a></li> -->
+</ul>
+</div>
+<nav>
+<center><a href="//discord.gg/piratesite">Discord</a>&nbsp;</center>
+</nav>
+<!-- <div style="position: fixed;bottom: 10px;right: 10px;color:white;font-weight: bold;opacity: 0.2;">Design/Coding by: <span style="color:rgb(148, 34, 34);">DeagleM#3127</span></div> -->
+</body>
+</html>
